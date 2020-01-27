eb3c0efbc7f6953d55ecfaab54c4b6914ded5f67
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2020-01-27 18:08:56 +0530

    remove invalid cases

--- __fixtures__/transforms css and glob/output.js
+++ __fixtures__/transforms css and glob/output.js
@@ -3,10 +3,4 @@ import { css as _css } from "goober";
 
 /*#__PURE__*/
 _css`color:red;padding:2px 4px;`;
-
-/*#__PURE__*/
-_css(`color:red;padding:2px 4px;`);
-
 _glob`color:red;padding:2px 4px;`;
-
-_glob(`color:red;padding:2px 4px;`);
